bece15c95235c1bbc42d811fd36c2dc98e82e04a
Swethapasu <swethapasupathi.367@gmail.com>
2026-08-17 14:55:41 +0530

    Add files via upload

---
+++ 114_Assignment_task/assignment1.py
@@ -0,0 +1,57 @@
+print("task1")
+print("Swetha\nCSE-AIDE\nJain university")
+
+print("task2")
+for i in range(3):
+    for j in range(7):
+        print("*",end="")
+    print("")
+        
+
+print("task3")
+print("Python is easy")
+
+print("task4")
+pattern=""
+for i in range(1,6):
+    pattern+=str(i)
+    print(pattern)
+
+print("task5")
+print("Computer\nScience\nEngineering")
+
+print("task6")
+print("Name:Swetha\tAge:19\tCity:Coimbatore")
+
+print("task7")
+length=int(input("enter length:"))
+breadth=int(input("enter breadth:"))
+print("Area:",length*breadth)
+
+print("task8")
+"""this program demonstrates
+multi-line comments in Python
+by calculating the sum of two numbers.
+"""
+a=10
+b=20
+print("Sum:",a+b)
+
+print("task9")
+#comment1:Display greeting
+print("Hello!")
+#comment2:Display status
+print("Learning python basics.")
+#Comment3:Display exit
+print("Goodbye!")
+
+print("task10")
+name="Swetha"
+print(name)
+
+print("task11")
+a=10
+b=20
+
+print(a)
+print(b)
